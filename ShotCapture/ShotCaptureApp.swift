fb1906b6d97ed5cc3bd9c86b6d48f7b7d7228979
Karma <karmjit435@gmail.com>
2026-07-11 23:02:01 +1000

    Initial Commit

---
+++ ShotCapture/ShotCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotCaptureApp.swift
+//  ShotCapture
+//
+//  Created by Karmjit Singh on 11/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShotCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShotCaptureTests/ShotCaptureTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShotCaptureTests.swift
+//  ShotCaptureTests
+//
+//  Created by Karmjit Singh on 11/7/2026.
+//
+
+import Testing
+@testable import ShotCapture
+
+struct ShotCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
